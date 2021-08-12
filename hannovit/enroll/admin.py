f641bfdf26836e596b4ea8d8a6b5b891165849e6
Bhaskar Mahor <51980844+mahorbhaskar@users.noreply.github.com>
2021-08-12 17:38:14 +0530

    Delete hannovit directory

--- hannovit/enroll/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User
-# Register your models here.
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display=('id','name','pan','age','gender','email','city')
